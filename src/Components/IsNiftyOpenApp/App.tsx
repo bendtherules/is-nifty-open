9537963e1b27f818e1dc7f3e96afa6eea4aa298c
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2018-03-09 19:31:49 +0530

    Remove placeholder css and html from app, add OpenToday child within container

--- src/Components/IsNiftyOpenApp/App.tsx
+++ src/Components/IsNiftyOpenApp/App.tsx
@@ -3,20 +3,13 @@ import './App.css';
 import { OpenToday } from '../OpenToday';
 import { allHolidays } from '../../Data';
 
-const logo = require('./logo.svg');
-
 class IsNiftyOpenApp extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <OpenToday question={OpenToday.OpenOrClose.Open} allHolidays={allHolidays}/>
+        <div className="OpenToday-container">
+          <OpenToday question={OpenToday.OpenOrClose.Open} allHolidays={allHolidays} />
+        </div>
       </div>
     );
   }
