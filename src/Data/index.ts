22d1cd0aa13454b7d8cfe77c27283b5e8c1ceda6
Abhas Bhattacharya <abhasbhattacharya2@gmail.com>
2019-01-08 10:12:29 +0530

    Update holiday list for 2019
    Source:
    https://www.nseindia.com/content/circulars/NMF39671.pdf
    https://zerodha.com/z-connect/traders-zone/holidays/trading-holidays-2019-nse-bse-mcx

--- src/Data/index.ts
+++ src/Data/index.ts
@@ -11,22 +11,21 @@ export type HolidayList = Array<Holiday>;
 
 export const allHolidays: HolidayList =
     [
-        { "Description": "Republic Day", "Date": parseDateInIndiaTZ("2018-1-26"), "Day": "Friday" },
-        { "Description": "Mahashivratri", "Date": parseDateInIndiaTZ("2018-2-13"), "Day": "Tuesday" },
-        { "Description": "Holi", "Date": parseDateInIndiaTZ("2018-3-2"), "Day": "Friday" },
-        { "Description": "Mahavir Jayanti", "Date": parseDateInIndiaTZ("2018-3-29"), "Day": "Thursday" },
-        { "Description": "Good Friday", "Date": parseDateInIndiaTZ("2018-3-30"), "Day": "Friday" },
-        { "Description": "Maharashtra Day", "Date": parseDateInIndiaTZ("2018-5-1"), "Day": "Tuesday" },
-        { "Description": "Independence Day", "Date": parseDateInIndiaTZ("2018-8-15"), "Day": "Wednesday" },
-        { "Description": "Bakri ID", "Date": parseDateInIndiaTZ("2018-8-22"), "Day": "Wednesday" },
-        { "Description": "Ganesh Chaturthi", "Date": parseDateInIndiaTZ("2018-9-13"), "Day": "Thursday" },
-        { "Description": "Moharram", "Date": parseDateInIndiaTZ("2018-9-20"), "Day": "Thursday" },
-        { "Description": "Mahatama Gandhi Jayanti", "Date": parseDateInIndiaTZ("2018-10-2"), "Day": "Tuesday" },
-        { "Description": "Dasera", "Date": parseDateInIndiaTZ("2018-10-18"), "Day": "Thursday" },
-        { "Description": "Diwali-Laxmi Pujan", "Date": parseDateInIndiaTZ("2018-11-7"), "Day": "Wednesday" },
-        { "Description": "Diwali-Balipratipada", "Date": parseDateInIndiaTZ("2018-11-8"), "Day": "Thursday" },
-        { "Description": "Gurunanak Jayanti", "Date": parseDateInIndiaTZ("2018-11-23"), "Day": "Friday" },
-        { "Description": "Christmas", "Date": parseDateInIndiaTZ("2018-12-25"), "Day": "Tuesday" },
+        { "Description": "Mahashivratri", "Date": parseDateInIndiaTZ("2019-3-04"), "Day": "Monday" },
+        { "Description": "Holi", "Date": parseDateInIndiaTZ("2019-3-21"), "Day": "Thursday" },
+        { "Description": "Mahavir Jayanti", "Date": parseDateInIndiaTZ("2019-4-17"), "Day": "Wednesday" },
+        { "Description": "Good Friday", "Date": parseDateInIndiaTZ("2019-4-19"), "Day": "Friday" },
+        { "Description": "Maharashtra Day", "Date": parseDateInIndiaTZ("2019-5-1"), "Day": "Wednesday" },
+        { "Description": "Id-Ul-Fitar (Ramzan ID)", "Date": parseDateInIndiaTZ("2019-6-05"), "Day": "Wednesday" },
+        { "Description": "Bakri ID", "Date": parseDateInIndiaTZ("2019-8-12"), "Day": "Monday" },
+        { "Description": "Independence Day", "Date": parseDateInIndiaTZ("2019-8-15"), "Day": "Thursday" },
+        { "Description": "Ganesh Chaturthi", "Date": parseDateInIndiaTZ("2019-9-02"), "Day": "Monday" },
+        { "Description": "Moharram", "Date": parseDateInIndiaTZ("2019-9-10"), "Day": "Tuesday" },
+        { "Description": "Mahatama Gandhi Jayanti", "Date": parseDateInIndiaTZ("2019-10-2"), "Day": "Wednesday" },
+        { "Description": "Dasera", "Date": parseDateInIndiaTZ("2019-10-08"), "Day": "Tuesday" },
+        { "Description": "Diwali-Balipratipada", "Date": parseDateInIndiaTZ("2019-10-28"), "Day": "Monday" },
+        { "Description": "Gurunanak Jayanti", "Date": parseDateInIndiaTZ("2019-11-12"), "Day": "Tuesday" },
+        { "Description": "Christmas", "Date": parseDateInIndiaTZ("2019-12-25"), "Day": "Wednesday" },
     ];
 
 function parseDateInIndiaTZ(dateString: string): moment.Moment {
